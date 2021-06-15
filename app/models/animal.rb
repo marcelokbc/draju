423f984f5de3130567b708164030ab9efd0297fa
Marcelo <marcelo.cansilva@gmail.com>
2021-06-14 21:28:07 -0300

    Validates models

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,5 @@
 class Animal < ApplicationRecord
+  belongs_to :appointments
+
+  validate :name, :age, :specie, presence: true
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,5 @@
 class Appointment < ApplicationRecord
+  has_many :animals
+
+  validate :name, :datetime, :address, :phone, presence: true
 end
